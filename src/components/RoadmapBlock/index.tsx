3f7eb73398eea96288846dbca66ec779df197cc7
Kamil Kiedrzyn <kamil.kiedrzyn@gmail.com>
2022-05-03 21:32:47 +0200

    Remove fade from roadmap

--- src/components/RoadmapBlock/index.tsx
+++ src/components/RoadmapBlock/index.tsx
@@ -4,7 +4,6 @@ import { Row, Col, Modal } from "antd";
 import { withTranslation } from "react-i18next";
 
 import { RoadmapBlockProps } from "./types";
-import { Fade } from "react-awesome-reveal";
 import {
   RoadmapBlockContainer,
   Content,
@@ -41,77 +40,75 @@ const RoadmapBlock = ({
   });
   return (
     <RoadmapBlockContainer id={id}>
-      <Fade direction="right">
-        <Row justify="center" align="middle">
-          <Col>
-            <h4>{t(title)}</h4>
-            <Content>{t(content)}</Content>
-            <ContentWrapper>
-              <Modal
-                // title={modalContent.title}
-                visible={visible}
-                keyboard={true}
-                footer={null}
-                onCancel={() => setVisible(false)}
-              >
-                <ModalWrapper>
-                  {/* <Row justify="space-between" align="middle"> */}
-                  <Row justify="center" align="middle">
-                    <Col lg={20} md={20} sm={20} xs={20}>
-                      <KanbanColumnHeader>
-                        {modalContent.title}
-                      </KanbanColumnHeader>
-                      <ProgressBar
-                        bgcolor="black"
-                        completed={modalContent.phase}
-                      />
-                      {modalContent.more}
-                    </Col>
-                  </Row>
-                </ModalWrapper>
-              </Modal>
-              <KanbanBoard>
-                <KanbanColumn>
-                  <KanbanColumnHeader>Pipeline</KanbanColumnHeader>
-                  <KanbanCard> Here is what we plan </KanbanCard>
+      <Row justify="center" align="middle">
+        <Col>
+          <h4>{t(title)}</h4>
+          <Content>{t(content)}</Content>
+          <ContentWrapper>
+            <Modal
+              // title={modalContent.title}
+              visible={visible}
+              keyboard={true}
+              footer={null}
+              onCancel={() => setVisible(false)}
+            >
+              <ModalWrapper>
+                {/* <Row justify="space-between" align="middle"> */}
+                <Row justify="center" align="middle">
+                  <Col lg={20} md={20} sm={20} xs={20}>
+                    <KanbanColumnHeader>
+                      {modalContent.title}
+                    </KanbanColumnHeader>
+                    <ProgressBar
+                      bgcolor="black"
+                      completed={modalContent.phase}
+                    />
+                    {modalContent.more}
+                  </Col>
+                </Row>
+              </ModalWrapper>
+            </Modal>
+            <KanbanBoard>
+              <KanbanColumn>
+                <KanbanColumnHeader>Pipeline</KanbanColumnHeader>
+                <KanbanCard> Here is what we plan </KanbanCard>
 
-                  {typeof roadmapItems === "object" &&
-                    roadmapItems.map((item: any, id: number) => {
-                      return (
-                        <KanbanCard>
-                          <Overlay
-                            onClick={() =>
-                              openModal({
-                                title: item.title,
-                                more: item.more,
-                                phase: item.phase,
-                              })
-                            }
-                          >
-                            <OverlayAnchor>More</OverlayAnchor>
-                          </Overlay>
-                          {/* <SvgIcon src={item.icon} width="60px" height="60px" /> */}
-                          <MinTitle>{t(item.title)}</MinTitle>
-                          <MinPara>{t(item.content)}</MinPara>
-                          <Row>
-                            {typeof item.tags === "object" &&
-                              item.tags.map((item: any, id: number) => {
-                                return <Tag>{t(item)}</Tag>;
-                              })}
-                          </Row>
-                        </KanbanCard>
-                      );
-                    })}
-                </KanbanColumn>
-                <KanbanColumn>
-                  <KanbanColumnHeader>Completed</KanbanColumnHeader>
-                  <KanbanCard> This is what w've done </KanbanCard>
-                </KanbanColumn>
-              </KanbanBoard>
-            </ContentWrapper>
-          </Col>
-        </Row>
-      </Fade>
+                {typeof roadmapItems === "object" &&
+                  roadmapItems.map((item: any, id: number) => {
+                    return (
+                      <KanbanCard>
+                        <Overlay
+                          onClick={() =>
+                            openModal({
+                              title: item.title,
+                              more: item.more,
+                              phase: item.phase,
+                            })
+                          }
+                        >
+                          <OverlayAnchor>More</OverlayAnchor>
+                        </Overlay>
+                        {/* <SvgIcon src={item.icon} width="60px" height="60px" /> */}
+                        <MinTitle>{t(item.title)}</MinTitle>
+                        <MinPara>{t(item.content)}</MinPara>
+                        <Row>
+                          {typeof item.tags === "object" &&
+                            item.tags.map((item: any, id: number) => {
+                              return <Tag>{t(item)}</Tag>;
+                            })}
+                        </Row>
+                      </KanbanCard>
+                    );
+                  })}
+              </KanbanColumn>
+              <KanbanColumn>
+                <KanbanColumnHeader>Completed</KanbanColumnHeader>
+                <KanbanCard> This is what w've done </KanbanCard>
+              </KanbanColumn>
+            </KanbanBoard>
+          </ContentWrapper>
+        </Col>
+      </Row>
     </RoadmapBlockContainer>
   );
 };
